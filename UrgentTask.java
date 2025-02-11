b302ac9342fa99bee1cde513e4011a450fb9f6da
Syrym <syrym.baidaly@gmail.com>
2025-02-11 09:07:54 +0500

    Added UrgentTask.java

---
+++ UrgentTask.java
@@ -0,0 +1,21 @@
+public class UrgentTask extends Task {
+    private String deadline;
+
+    // Конструктор
+    public UrgentTask(String title, String description, String deadline) {
+        super(title, description);
+        this.deadline = deadline;
+    }
+
+    // Геттер
+    public String getDeadline() {
+        return deadline;
+    }
+
+    // Переопределение метода printTask() (полиморфизм)
+    @Override
+    public void printTask() {
+        super.printTask();
+        System.out.println("Deadline: " + deadline);
+    }
+}
